a17ca12abb92b84f5922a9128a4666e121c843a2
zackbam <zackbam@gmail.com>
2017-04-05 13:39:27 +0200

    Add files via upload

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -62,35 +62,10 @@ void ofApp::mouseMoved(int x, int y) {
 	}
 }
 
-
-//--------------------------------------------------------------
-void ofApp::update(){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::draw(){
-	text << "value: " << midiMessage.value;
-	ofDrawBitmapString(text.str(), 20, 20);
-	text.str(""); // clear
-	if (midiMessage.status == MIDI_PITCH_BEND) {
-		ofDrawRectangle(20, 50, ofMap(midiMessage.value, 0, MIDI_MAX_BEND, 0, ofGetWidth() - 40), 20);
-	}
-	else {
-		ofDrawRectangle(20, 50, ofMap(midiMessage.value, 0, 127, 0, ofGetWidth() - 40), 20);
-	}
-
-	
-	for (int i = 0; i < notesNumber; i++) {
-		ofLine(ofPoint(i*noteWidth, 0), ofPoint(i*noteWidth, ofGetHeight()));
-	}
-}
-
 void ofApp::newMidiMessage(ofxMidiMessage& msg) {
 	// make a copy of the latest message
-	
+	midiMessage = msg;
 	if (msg.status == MIDI_CONTROL_CHANGE) {
-		midiMessage = msg;
 		breath = msg.value;
 		if (breath < thr) {
 			midiOut.sendNoteOff(1, scale[curNote], 0);
@@ -105,6 +80,60 @@ void ofApp::newMidiMessage(ofxMidiMessage& msg) {
 		}
 	}
 }
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+	ofSetColor(0);
+
+	// draw the last recieved message contents to the screen
+	text << "Received: " << ofxMidiMessage::getStatusString(midiMessage.status);
+	ofDrawBitmapString(text.str(), 20, 20);
+	text.str(""); // clear
+
+	text << "channel: " << midiMessage.channel;
+	ofDrawBitmapString(text.str(), 20, 34);
+	text.str(""); // clear
+
+	text << "pitch: " << midiMessage.pitch;
+	ofDrawBitmapString(text.str(), 20, 48);
+	text.str(""); // clear
+	ofDrawRectangle(20, 58, ofMap(midiMessage.pitch, 0, 127, 0, ofGetWidth() - 40), 20);
+
+	text << "velocity: " << midiMessage.velocity;
+	ofDrawBitmapString(text.str(), 20, 96);
+	text.str(""); // clear
+	ofDrawRectangle(20, 105, ofMap(midiMessage.velocity, 0, 127, 0, ofGetWidth() - 40), 20);
+
+	text << "control: " << midiMessage.control;
+	ofDrawBitmapString(text.str(), 20, 144);
+	text.str(""); // clear
+	ofDrawRectangle(20, 154, ofMap(midiMessage.control, 0, 127, 0, ofGetWidth() - 40), 20);
+
+	text << "value: " << midiMessage.value;
+	ofDrawBitmapString(text.str(), 20, 192);
+	text.str(""); // clear
+	if (midiMessage.status == MIDI_PITCH_BEND) {
+		ofDrawRectangle(20, 202, ofMap(midiMessage.value, 0, MIDI_MAX_BEND, 0, ofGetWidth() - 40), 20);
+	}
+	else {
+		ofDrawRectangle(20, 202, ofMap(midiMessage.value, 0, 127, 0, ofGetWidth() - 40), 20);
+	}
+
+	text << "delta: " << midiMessage.deltatime;
+	ofDrawBitmapString(text.str(), 20, 240);
+	text.str(""); // clear
+	ofSetColor(255);
+	for (int i = 0; i < notesNumber; i++) {
+		ofLine(ofPoint(i*noteWidth, 0), ofPoint(i*noteWidth, ofGetHeight()));
+	}
+}
+
 
 void ofApp::exit() {
 	midiOut.closePort();
